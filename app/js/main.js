0489a4447af59ce376a2a2655c60bf2c86f19fdc
Burkhardt30 <burkhardt30tto@gmail.com>
2021-10-19 15:40:30 +0500

    added services

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+	$('.services__tab-link').on('click', function(e) {
+		e.preventDefault()
+		$('.services__tab-link').removeClass('services__tab-link--active')
+		$('.services__image').removeClass('services__image--show')
+		$(this).addClass('services__tab-link--active')
+		$($(this).attr('href')).addClass('services__image--show')
+	})
 });
