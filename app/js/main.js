9ebc8ab6a79bd15f6a260fcbce13984cfcfdf5b3
Burkhardt30 <burkhardt30tto@gmail.com>
2021-10-30 20:55:20 +0500

    added blog, how we work, cases, testimonials + fixed height of all sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,26 @@
 $(function(){
+
+  $('.testimonials__slider-inner').slick({
+    arrows: false,
+    draggable: false,
+  })
+  $('.testimonials__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider-inner').slick('slickPrev')
+  })
+  $('.testimonials__next').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider-inner').slick('slickNext')
+  })
+
+	$('.blog__tab').on('click', function(e) {
+		e.preventDefault()
+		$('.blog__tab').removeClass('blog__tab--active')
+		$('.blog__item').removeClass('blog__item--show')
+		$(this).addClass('blog__tab--active')
+		$($(this).attr('href')).addClass('blog__item--show')
+	})
+  
 	$('.services__tab-link').on('click', function(e) {
 		e.preventDefault()
 		$('.services__tab-link').removeClass('services__tab-link--active')
@@ -6,4 +28,5 @@ $(function(){
 		$(this).addClass('services__tab-link--active')
 		$($(this).attr('href')).addClass('services__image--show')
 	})
-});
+
+})
