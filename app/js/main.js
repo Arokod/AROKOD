9aec860fee22c7eaab706f4f9c7579875f306e26
Burkhardt30 <burkhardt30tto@gmail.com>
2021-10-31 17:13:53 +0500

    added team, contact, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
-$(function(){
+$(function () {
+
+  const textarea = document.querySelector('.contact__form-textarea')
+  textarea.addEventListener('input', () => {
+    textarea.scrollTop > 0 ?
+      textarea.style.height = textarea.scrollHeight + "px" :
+      textarea.style.height = 'auto'
+  })
+
+  $('.team__slider-inner').slick({
+    arrows: false,
+    slidesToShow: 3,
+    infinite: false,
+    variableWidth: true,
+  })
 
   $('.testimonials__slider-inner').slick({
     arrows: false,
@@ -13,20 +27,20 @@ $(function(){
     $('.testimonials__slider-inner').slick('slickNext')
   })
 
-	$('.blog__tab').on('click', function(e) {
-		e.preventDefault()
-		$('.blog__tab').removeClass('blog__tab--active')
-		$('.blog__item').removeClass('blog__item--show')
-		$(this).addClass('blog__tab--active')
-		$($(this).attr('href')).addClass('blog__item--show')
-	})
-  
-	$('.services__tab-link').on('click', function(e) {
-		e.preventDefault()
-		$('.services__tab-link').removeClass('services__tab-link--active')
-		$('.services__image').removeClass('services__image--show')
-		$(this).addClass('services__tab-link--active')
-		$($(this).attr('href')).addClass('services__image--show')
-	})
+  $('.blog__tab').on('click', function (e) {
+    e.preventDefault()
+    $('.blog__tab').removeClass('blog__tab--active')
+    $('.blog__item').removeClass('blog__item--show')
+    $(this).addClass('blog__tab--active')
+    $($(this).attr('href')).addClass('blog__item--show')
+  })
+
+  $('.services__tab-link').on('click', function (e) {
+    e.preventDefault()
+    $('.services__tab-link').removeClass('services__tab-link--active')
+    $('.services__image').removeClass('services__image--show')
+    $(this).addClass('services__tab-link--active')
+    $($(this).attr('href')).addClass('services__image--show')
+  })
 
 })
